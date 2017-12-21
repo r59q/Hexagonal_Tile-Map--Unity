b909f30d9d05c3fbd7b14840b432a54a6cfaa774
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2017-12-21 21:19:49 +0100

    v0.12 - Perlin noise map generator
    + Added PerlinGenerator
    
    PerlinGenerator
        + Added the ability to generate noise map.
        + Added noise based height to tiles.

--- Assets/Scripts/MapGenerators/PerlinGenerator.cs
+++ Assets/Scripts/MapGenerators/PerlinGenerator.cs
@@ -7,7 +7,64 @@ namespace HexaMap.Generators
     public class PerlinGenerator : SimpleGenerator
     {
 
+        public float maxHeight = 3;
 
+        public float noiseScale = 15.2f;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            // prework
+            float[,] noiseMap = GetNoiseMap((int)tileMap.size.x, (int)tileMap.size.y, noiseScale);
+
+            InstantiateAll();
+
+            // work
+            // give tiles height
+            for (int x = 0; x < tileMap.size.x; x++)
+            {
+                for (int y = 0; y < tileMap.size.y; y++)
+                {
+                    Tile currentTile = tileMap.Tile(new Vector2(x, y));
+                    currentTile.Height(maxHeight * noiseMap[x, y]);
+                }
+            }
+
+            // debugging
+            for (int x = 0; x < noiseMap.GetLength(0); x++)
+            {
+                for (int y = 0; y < noiseMap.GetLength(1); y++)
+                {
+                    //print(noiseMap[x, y]);
+                }
+            }
+        }
+
+
+        float[,] GetNoiseMap(int mapWidth, int mapHeight, float scale)
+        {
+            float[,] noiseMap = new float[mapWidth, mapHeight];
+
+            if (scale <= 0)
+            {
+                scale = 0.0001f;
+            }
+
+            for (int x = 0; x < mapWidth; x++)
+            {
+                for (int y = 0; y < mapHeight; y++)
+                {
+                    float sampleX = x / scale;
+                    float sampleY = y / scale;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                    noiseMap[x, y] = perlinValue;
+                }
+            }
+
+            return noiseMap;
+        }
 
     }
 }
